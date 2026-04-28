8239bcfb1bb138707bf8459e5f919f4b3d779171
muhammadaziz1911 <azizbekmaxmudov22@gmail.com>
2026-04-28 22:45:40 +0500

    update

---
+++ src/components/pages/profile/Profile.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Profile = () => {
+
+  const { data, isLoadin } = useQuery({
+    queryKey: ['userData'],
+    queryFn: () => request.get('/users/me').then((res) => res.data)
+  })
+
+  const { data: userData, isLoading } = useQuery({
+    queryKey: ['userData'],
+    queryFn: () => request.get('/users/me').then((res) => res.data)
+})
+
+const { register, handleSubmit, formState: { errors } } = useForm()
+
+const { mutate } = useMutation({
+    mutationKey: ['user', userData?.data?.user_id],
+    mutationFn: (data) => request.put(`/users/${userData?.data?.user_id}`, data),
+    onSuccess: () => {
+        toast.success('User updated successfully')
+    },
+    onError: () => {
+        toast.error('User updated failed')
+    }
+})
+
+const onSubmit = (data) => {
+    mutate(data)
+}
+
+
+  return (
+    <div>
+      
+
+
+    </div>
+  )
+}
+
+export default Profile
